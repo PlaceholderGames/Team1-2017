7bf3ca16082070588c777cc42acb4bb312710f4a
SharktallicA <22660310+SharktallicA@users.noreply.github.com>
2017-11-14 16:48:15 +0000

    Replace orbit drawer in new attempt

--- Assets/Scripts/OrbitDrawer.cs
+++ Assets/Scripts/OrbitDrawer.cs
@@ -4,37 +4,16 @@ using UnityEngine;
 
 public class OrbitDrawer : MonoBehaviour
 {
-    // Creates a line renderer that follows a Sin() function
-    // and animates it.
-
-    public Color c1 = Color.yellow;
-    public Color c2 = Color.red;
-    public int lengthOfLineRenderer = 2000;
-
     void Start()
     {
         LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
-        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
-        lineRenderer.widthMultiplier = 0.9f;
-        lineRenderer.positionCount = lengthOfLineRenderer;
-
-        // A simple 2 color gradient with a fixed alpha of 1.0f.
-        float alpha = 1.0f;
-        Gradient gradient = new Gradient();
-        gradient.SetKeys(
-            new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
-            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
-            );
-        lineRenderer.colorGradient = gradient;
     }
 
     void Update()
     {
         LineRenderer lineRenderer = GetComponent<LineRenderer>();
-        var t = Time.time;
-        for (int i = 0; i < lengthOfLineRenderer; i++)
-        {
-            lineRenderer.SetPosition(i, new Vector3(i * 0.5f, Mathf.Sin(i + t), 0.0f));
-        }
+        lineRenderer.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+        lineRenderer.SetColors(Color.black, Color.black);
+
     }
 }
