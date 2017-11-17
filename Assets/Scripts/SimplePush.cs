a2cfda0672ff87ec827667a7816e40cb54133903
SharktallicA <22660310+SharktallicA@users.noreply.github.com>
2017-11-17 12:56:02 +0000

    Add basic mover as placeholder

---
+++ Assets/Scripts/SimplePush.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+/// PLACEHOLDER MOVEMENT script
+/// </summary>
+/// 
+using UnityEngine;
+
+public class SimplePush : MonoBehaviour
+{
+    public float fltVelocity = 1000f;
+    public float fltStepper = 10f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+
+        Vector3 vctMovement = new Vector3(fltStepper, 0.0f, 0.0f);
+
+        rb.AddForce(vctMovement * fltVelocity);
+	}
+}
