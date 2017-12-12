c94a729d376941e155db2de51f398895c279c2eb
Probert L (FCES) <16009797@glam.ac.uk>
2017-12-12 17:02:33 +0000

    Milestone v1.45

---
+++ Assets/Scripts/AnyKeyContinue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnyKeyContinue : MonoBehaviour
+{
+
+    public GameObject MainMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            gameObject.SetActive(false);
+            MainMenu.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/SetAudioLevels.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetAudioLevels : MonoBehaviour {
+
+	public AudioMixer NewMixer;					//Used to hold a reference to the AudioMixer mainMixer
+
+    public void SetMasterLevel(float masterLvl)
+    {
+        NewMixer.SetFloat("masterVol", masterLvl);
+    }
+
+    //Call this function and pass in the float parameter musicLvl to set the volume of the AudioMixerGroup Music in mainMixer
+    public void SetMusicLevel(float musicLvl)
+	{
+		NewMixer.SetFloat("musicVol", musicLvl);
+	}
+
+	//Call this function and pass in the float parameter sfxLevel to set the volume of the AudioMixerGroup SoundFx in mainMixer
+	public void SetSfxLevel(float SFXLvl)
+	{
+		NewMixer.SetFloat("SFXVol", SFXLvl);
+	}
+}
