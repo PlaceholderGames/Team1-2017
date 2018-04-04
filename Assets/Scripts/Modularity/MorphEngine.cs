730f3bf64c2767fd1e776284ba2b776be55a3780
SharktallicA <22660310+SharktallicA@users.noreply.github.com>
2018-04-04 13:25:20 +0100

    Prototype modularity script, alterations to how tiering will work
    Changelog:
    - MorphEngine: allows the probe to have its tier set and basic transformations in scale applied. A debug method DebugMorph() is available to allow testing of the morph with number keys 1 through 4
    - ProbeObject: to make upgrading a bit simpler but more intuitive, only one tier number is being held by the probe. Once the rest of modularity stuff is working, GetPowerGeneration() and GetSensorCapacity() will be reimplemented

---
+++ Assets/Scripts/Modularity/MorphEngine.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Allows tier changing and scale transformations to be applied to the probe
+/// </summary>
+public class MorphEngine : MonoBehaviour
+{
+    /// <summary>
+    /// Local reference of probe's object class
+    /// </summary>
+    private ProbeObject probe;
+
+    /// <summary>
+    /// Division factor to alter morphing factor calculations
+    /// </summary>
+    [SerializeField]
+    private int MorphModifier = 4;
+
+    void Start() { probe = GameObject.FindGameObjectWithTag("Player").GetComponent<ProbeObject>(); }    
+
+    void Update() { DebugMorph(); }
+
+    /// <summary>
+    /// Sets the tier of the probe
+    /// </summary>
+    /// <param name="tier">Tier in which the probe will be set to (defaulted as 1)</param>
+    void SetTier(float tier = 1)
+    {
+        //if specified change is the the same as the current tier, ignore attempting to morph
+        if (probe.GetTier() == tier) return;
+
+        //calculate morphFactor (scale in which certain probe features will be)
+        float morphFactor = 1f + (float)(tier / MorphModifier);
+        probe.SetTier((int)tier);
+
+        //call for the application of the scale changes
+        ApplyMorph(morphFactor);
+    }
+
+    /// <summary>
+    /// Applies a scale transform to tagged parts of the probe
+    /// </summary>
+    /// <param name="morphFactor">New scale</param>
+    void ApplyMorph(float morphFactor)
+    {
+        GameObject.FindGameObjectWithTag("ProbePylon").transform.localScale = new Vector3(morphFactor, morphFactor, 1);
+        GameObject.FindGameObjectWithTag("ProbeAerial").transform.localScale = new Vector3(morphFactor, morphFactor, morphFactor);
+        GameObject.FindGameObjectWithTag("ProbeEngine").transform.localScale = new Vector3(morphFactor, morphFactor, morphFactor);
+        GameObject.FindGameObjectWithTag("Gun").transform.localScale = new Vector3(morphFactor, morphFactor, morphFactor);
+    }
+
+    /// <summary>
+    /// Allows testing of the morph with number keys 1 to 4 (TO BE DISABLED IN THE FUTURE)
+    /// </summary>
+    void DebugMorph()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+            SetTier(1);
+        else if (Input.GetKey(KeyCode.Alpha2))
+            SetTier(2);
+        else if (Input.GetKey(KeyCode.Alpha3))
+            SetTier(3);
+        else if (Input.GetKey(KeyCode.Alpha4))
+            SetTier(4);
+    }
+}
--- Assets/Scripts/ObjectClasses/ProbeObject.cs
+++ Assets/Scripts/ObjectClasses/ProbeObject.cs
@@ -10,39 +10,28 @@ public class ProbeObject : GeneralObject
     //probe module stats
     private float CommunicationDistance = 10000.0f; //distance the probe can communicate over
     private float Fuel = 10000.0f; //range the probe can travel before being stranded
-    private int PowerGenerationBonus = 0; //bonus from power generation
-    private int SensorCapacityBonus = 0; //bonus from sensor capacity
-
-    //probe tiering stats
-    private int CATier = 1; //communications array tier
-    private int FTTier = 1; //fuel tank tier
-    private int PGTier = 1; //power generator tier
-    private int SSTier = 1; //sensor suite tier
-    private int OverallTier = 1; //overall tier of probe
 
     //other stats
+    private int Tier = 1; //overall tier of probe
     private int Munitions = 100; //amount of projectiles the probe can hold
     private int Materials = 0; //amount of materials the probe is carrying
 
     //override Start to allow ProbeObject to calculate its average tier
-    override public void DerivedStart() { OverallTier = (int)System.Math.Round((double)((CATier + FTTier + PGTier + SSTier) / 4)); } 
+    override public void DerivedStart() { Tier = 1; } 
 
     //variable getters
     public float GetCommunicationDistance() { return CommunicationDistance; }
     public float GetFuel() { return Fuel; }
     public int GetFuelRounded() { return (int)System.Math.Round(Fuel); }
-    public float GetPowerGeneration() { return PowerGenerationBonus; }
-    public float GetSensorCapacity() { return SensorCapacityBonus; }
-    public int GetTier() { return OverallTier; }
+    public float GetPowerGeneration() { return 0; }
+    public float GetSensorCapacity() { return 0; }
+    public int GetTier() { return Tier; }
     public int GetMaterialsCount() { return Materials; }
     public int GetMunitionsRemaining() { return Munitions; }
 
     //variable setters
     public void SetFuel(float newFuelVal) { Fuel = newFuelVal; }
-    public void SetCommunicationArrayTier(int newCAVal) { CATier = newCAVal; }
-    public void SetFuelTankTier(int newFTVal) { FTTier = newFTVal; }
-    public void SetPowerGeneratorTier(int newPGVal) { PGTier = newPGVal; }
-    public void SetSensorSuiteTier(int newSSVal) { SSTier = newSSVal; }
+    public void SetTier(int newTier) { Tier = newTier; }
     public void SetMaterialsCount(int newMaterials) { Materials = newMaterials; }
     public void SetMunitionsRemaining(int newMunitions) { Munitions = newMunitions; }
 }
