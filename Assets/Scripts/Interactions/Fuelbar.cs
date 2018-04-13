38ab0ae8751155778b054a3263190279c05a6f7b
SharktallicA <22660310+SharktallicA@users.noreply.github.com>
2018-04-13 17:29:54 +0100

    Merged UI-Main manually
    Changelog:
    - Due to @ThallasTV's repo issues, I added his UI elements manually to master

---
+++ Assets/Scripts/Interactions/Fuelbar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fuelbar : MonoBehaviour {
+
+    private ProbeObject probe;
+    public Image CurrentProbeFuel;
+    public Text FuelPercentage;
+
+    private float StartFuel;
+    private float MaxFuel;
+
+    private void Start()
+    {
+        probe = GameObject.FindGameObjectWithTag("Player").GetComponent<ProbeObject>();
+        StartFuel = probe.GetFuel();
+        MaxFuel = StartFuel;
+    }
+
+    private void Update()
+    {
+        float pFuel = probe.GetFuel();
+        if (pFuel < 0) pFuel = 0;
+        float ratio = pFuel / MaxFuel;
+        CurrentProbeFuel.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        FuelPercentage.text = ((int)System.Math.Round((double)(ratio*100))).ToString() + '%';
+    }
+
+
+}
---
+++ Assets/Scripts/Interactions/Healthbar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour {
+
+    private ProbeObject probe;
+    public Image CurrentProbeHealth;
+    public Text HealthPercentage;
+
+    private float maxHitpoint = 100;
+
+    private void Start()
+    {
+        probe = GameObject.FindGameObjectWithTag("Player").GetComponent<ProbeObject>();
+    }
+
+    private void Update()
+    {
+        float pHealth = probe.Health;
+        if (pHealth < 0) pHealth = 0;
+        float ratio = pHealth / maxHitpoint;
+        CurrentProbeHealth.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        HealthPercentage.text = (ratio*100).ToString() + '%';
+    }
+    
+}
