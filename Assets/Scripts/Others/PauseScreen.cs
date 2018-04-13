3edaa9209b782b01ebef3c237ec4b416b5f22074
Luke <16009797@students.southwales.ac.uk>
2018-04-13 13:52:10 +0100

    V1.72.1 - Fresh build

---
+++ Assets/Scripts/Others/PauseScreen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScreen : MonoBehaviour {
+
+    public static bool gamePaused = false;
+
+    public GameObject pauseMenuUI;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (gamePaused)
+                Resume();
+            else
+                Pause();
+        }
+	}
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+       // Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+       // Time.timeScale = 0f;
+        gamePaused = true;
+    }
+}
