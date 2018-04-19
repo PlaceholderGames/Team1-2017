8cdf927e6175ee811f0622edead204eee6debcdc
SharktallicA <22660310+SharktallicA@users.noreply.github.com>
2018-04-19 12:27:08 +0100

    Make UI and camera adjustments
    Changelog:
    - Camera: position relative to player slightly altered based on feedback from @jakedpotato
    - UI: elements should now properly adjust based on different resolution, minigame enlarged slightly, fontsize of FPS counter and co-ord display reduced slightly for better scaling
    - Pause menu has been disabled pending aid from @16009797 to fix issues with it

--- Assets/Scripts/Others/fps.cs
+++ Assets/Scripts/Others/fps.cs
@@ -19,7 +19,7 @@ public class fps : MonoBehaviour {
 
         Rect rect = new Rect(0, 0, w, h * 2 / 100);
         style.alignment = TextAnchor.UpperLeft;
-        style.fontSize = h * 2 / 100;
+        style.fontSize = h * 1 / 75;
         style.normal.textColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
         float msec = deltaTime * 1000.0f;
         float fps = 1.0f / deltaTime;
