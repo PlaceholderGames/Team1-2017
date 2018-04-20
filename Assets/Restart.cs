405b9ceac82d6ffeb1f6e63aadf9886ab4309fd2
SharktallicA <22660310+SharktallicA@users.noreply.github.com>
2018-04-20 17:54:51 +0100

    Add respawn capabilities
    Changelog:
    - UI: improved @ThallasTV respawn menu and integrated it's capabilities

---
+++ Assets/Restart.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Restart : MonoBehaviour
+{
+   public ProbeObject Probe;
+   public GameObject fadeBlack;
+
+    void Start()
+    {
+        Probe = GameObject.FindGameObjectWithTag("Player").GetComponent<ProbeObject>();
+
+
+
+    }
+    void Update()
+    {
+        if (Probe == null)
+        {
+            this.GetComponent<CanvasGroup>().alpha = 1;
+            gameObject.GetComponent<Button>().onClick.AddListener(Restartgame);
+        }
+    }
+
+    void Restartgame()
+    {
+        fadeBlack.GetComponent<Animator>().SetTrigger("Fade");
+    }
+
+}
+
