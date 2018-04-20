746d6fd5640841673a31b248859446e1653ddada
Probert L (FCES) <16009797@glam.ac.uk>
2018-04-20 15:37:01 +0100

    Wired up Interface controls, synced main menu and pause menu

---
+++ Assets/Scripts/Others/PullPushSettings.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PullPushSettings : MonoBehaviour {
+
+    public static int gFOV = 75;
+    public static int gCamDis = -10;
+    public static bool gLenFlare = true;
+
+    public Slider FOVSlider;
+    public Slider CamDisSlider;
+    public Toggle LenFlareToggle;
+
+    public GameObject mainCamera;
+
+    // Use this for initialization
+    void Start () {
+
+        FOVSlider.value = gFOV;
+        CamDisSlider.value = gCamDis*(-1);
+        LenFlareToggle.isOn = gLenFlare;
+
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gFOV = (int)FOVSlider.value;
+        gCamDis = (int)CamDisSlider.value * (-1);
+        gLenFlare = LenFlareToggle.isOn;
+	}
+
+
+
+}
